0d8c530d0e2099c39743941de2dcc49bb27b2d70
eruetsch <eruetsch@trinity.edu>
2022-09-16 08:21:22 -0500

    cleaned up main.scala, added a bit of css

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -4,17 +4,12 @@ import org.scalajs.dom
 import slinky.core._
 import slinky.web.ReactDOM
 import slinky.web.html._
-import org.scalajs.dom.raw.WebSocket
 
 object Main {
   def main(args: Array[String]): Unit = {
-      println("Call the react stuff.")
       ReactDOM.render(
-        div(
-          TitleBot(), 
-        ),
-          dom.document.getElementById("root")
-        
+        div(TitleBot()),
+        dom.document.getElementById("root")
       )
 
   }
